0f08b0bf63321a6230ba1dc09183279fae502697
Tobias Fried <friedtm@gmail.com>
2020-04-29 16:45:53 -0400

    Extract smaller functions and improve logging logic

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import fs from "fs";
-import util from "util";
-import EnglishDictionary from "en-dictionary";
-import GetOpt from "node-getopt";
+const fs = require("fs");
+const util = require("util");
+const Dictionary = require("en-dictionary").default;
+const GetOpt = require("node-getopt");
 
-const Dictionary = EnglishDictionary.default;
 const help = `\
 Usage:
   node wordnet-builder [OPTION]... [WORD]...
@@ -28,25 +27,17 @@ const { argv, options } = opt;
 options.verbose && console.log(argv, options);
 
 if (options.file) handleInputFile(options.file);
-else if (argv.length) {
-  search(argv)
-    .then(parse)
-    .catch((error) => {
-      console.error(error);
-      process.exitCode = 1;
-    });
-} else console.error("No input provided");
+else if (argv.length) handleSearch(argv);
+else {
+  console.error("No input provided");
+  process.exitCode = 1;
+}
 
 async function handleInputFile(path) {
   if (fs.existsSync(path)) {
     try {
       const words = fs.readFileSync(path).toString().split("\n");
-      search(words)
-        .then(parse)
-        .catch((error) => {
-          console.error(error);
-          process.exitCode = 1;
-        });
+      handleSearch(words);
     } catch (e) {
       console.error(e);
       process.exitCode = 1;
@@ -57,89 +48,126 @@ async function handleInputFile(path) {
   }
 }
 
-async function search(words) {
+function handleSearch(words) {
+  lookup(words)
+    .then((entryMap) => {
+      if (options.raw) {
+        log(entryMap);
+        return;
+      }
+      handleParse(entryMap);
+    })
+    .catch((error) => {
+      console.error(error);
+      process.exitCode = 1;
+    });
+}
+
+async function lookup(words) {
   // TODO: figure out what's wrong with en-wordnet path
   const dictionary = new Dictionary(
-    "C:\\Users\\Tobias Fried\\Dropbox\\Personal\\code\\Nodejs\\wordnet-builder\\node_modules\\en-wordnet\\database\\3.1"
+    `${__dirname}/../node_modules/en-wordnet/database/3.1`
   );
   await dictionary.init();
   const result = dictionary.searchFor(words);
-
-  if (options.raw) console.info(util.inspect(result, { depth: null, colors: true }));
-  else return result;
+  // const result = dictionary.searchSimpleFor(words);
+  // const result = dictionary.searchOffsetsInDataFor([01337350]);
+  // const result = dictionary.searchOffsetsInDataFor([
+  //   01338411,
+  //   01337350,
+  //   5628738,
+  //   14355278,
+  //   1517459,
+  //   2126629,
+  // ]);
+  return result;
 }
 
-function parse(entryMap) {
+function handleParse(entryMap) {
   let entryObject = {};
   for (let [key, value] of entryMap) {
     entryObject[key] = value;
   }
-  if (options.human) {
-    const readable = Object.keys(entryObject).reduce((acc, word) => {
-      const { pos, offsetData } = entryObject[word];
-      acc[word] = {
-        pos,
-        senses: offsetData.map((sense) => ({
-          words: sense.words.join(", "),
-          pos: sense.pos,
-          definition: sense.glossary[0],
-        })),
-      };
-      return acc;
-    }, {});
-    output(readable);
-  } else {
-    const tokenSeparators = /[ ,()]/;
-    const network = Object.keys(entryObject).reduce(
-      (acc, word) => {
-        const { offsetData = [] } = entryObject[word];
-        const isValidToken = (token) =>
-          token.length && !token.match(/\d/) && token !== word;
 
-        for (let sense of offsetData) {
-          const definition = sense.glossary[0];
-          acc.responses.push({ source: word, value: definition });
-          acc.nodes.push(
-            ...[
-              ...new Set(
-                definition.split(tokenSeparators).filter(isValidToken)
-              ),
-            ].map((value) => ({ value }))
-          );
-          acc.edges.push(
-            ...[
-              ...new Set(
-                definition.split(tokenSeparators).filter(isValidToken)
-              ),
-            ].map((target) => ({ source: word, target }))
-          );
-        }
-        return acc;
-      },
-      { responses: [], nodes: [], edges: [] }
-    );
-    output(network);
+  if (options.human) {
+    parseHumanReadable(entryObject);
+    return;
   }
+
+  parseNetwork(entryObject);
+}
+
+function parseHumanReadable(entryObject) {
+  const readable = Object.keys(entryObject).reduce((acc, word) => {
+    const { pos, offsetData } = entryObject[word];
+    acc[word] = {
+      pos,
+      senses: offsetData.map((sense) => ({
+        words: sense.words.join(", "),
+        pos: sense.pos,
+        definition: sense.glossary[0],
+      })),
+    };
+    return acc;
+  }, {});
+  output(readable);
+}
+
+function parseNetwork(entryObject) {
+  const tokenSeparators = /[ ,()]/;
+  const network = Object.keys(entryObject).reduce(
+    (acc, word) => {
+      const { offsetData = [] } = entryObject[word];
+      const isValidToken = (token) =>
+        token.length && !token.match(/\d/) && token !== word;
+
+      for (let sense of offsetData) {
+        const definition = sense.glossary[0];
+        acc.responses.push({ source: word, value: definition });
+        acc.nodes.push(
+          ...[
+            ...new Set(definition.split(tokenSeparators).filter(isValidToken)),
+          ].map((value) => ({ value }))
+        );
+        acc.edges.push(
+          ...[
+            ...new Set(definition.split(tokenSeparators).filter(isValidToken)),
+          ].map((target) => ({ source: word, target }))
+        );
+      }
+      return acc;
+    },
+    { responses: [], nodes: [], edges: [] }
+  );
+  output(network);
 }
 
 function output(network) {
   if (options.output) {
     try {
       fs.writeFileSync(options.output, JSON.stringify(network, null, 2));
-      options.verbose && !options.raw && log(network);
+      options.verbose && log(network);
+      return;
     } catch (e) {
       console.error(e);
       process.exitCode = 1;
     }
-  } else {
-    !options.raw && log(network);
   }
+
+  log(network);
 }
 
 function log(contents) {
-  if (options.color) {
+  if (options.color || options.raw) {
     console.info(util.inspect(contents, { depth: null, colors: true }));
-  } else {
-    console.log(JSON.stringify(contents, null, 2));
+    return;
   }
+
+  console.log(JSON.stringify(contents, null, 2));
 }
+
+// upload()
+//   .then((result) =>
+//     console.info(util.inspect(result, { depth: null, colors: true }))
+//   )
+//   .catch(console.error);
